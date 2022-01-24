9f61aea7e7e16e5ca33b52868f91dee5722a9cd7
fabet <asilyk112@gmail.com>
2022-01-24 09:05:13 +0300

    Create DataManager class

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -5,6 +5,61 @@
 //  Created by Alexander on 24.01.2022.
 //
 
+class DataManager {
+    static let names = [
+        "Ivan",
+        "Aleksandr",
+        "Aleksei",
+        "Igor",
+        "Natalia",
+        "Anastasia",
+        "Uliana",
+        "Aleksandra",
+        "John",
+        "Aaron",
+        "Tim",
+        "Ted",
+        "Steven",
+        "Arthur"
+    ]
+
+    static let surnames = [
+        "Sokol",
+        "Oryol",
+        "Solovey",
+        "Sova",
+        "Zayac",
+        "Medved",
+        "Lisa",
+        "Volk",
+        "Smith",
+        "Dow",
+        "Isaacson",
+        "Pennyworth",
+        "Jankins",
+        "Black"
+    ]
+
+    static let phoneNumbers = Array(repeating: Int.random(in: 100_000_000...999_999_999), count: 14)
+
+    static let emails = [
+        "myopre@mail.ru",
+        "neemed@icloud.com",
+        "nitone@yandex.ru",
+        "postad@gmail.com",
+        "rightcl@mail.ru",
+        "shangbook@icloud.com",
+        "spunkysunset@yandex.ru",
+        "thegamm@gmail.com",
+        "apenguinblog@mail.ru",
+        "autopol@icloud.com",
+        "aveden@yandex.ru",
+        "beachem@gmail.com",
+        "beplay@mail.ru",
+        "bleminn@icloud.com",
+    ]
+}
+
 struct Person {
     let name: String
     let surname: String
@@ -16,40 +71,20 @@ struct Person {
     let phoneNumber: Int
     let email: String
 
-    static func getPersons() -> [Person] {
+    func getPersons() -> [Person] {
         var persons: [Person] = []
 
-        let names: Set<String> = [
-            "Ivan",
-            "Aleksandr",
-            "Aleksei",
-            "Igor",
-            "Natalia",
-            "Anastasia",
-            "Uliana",
-            "Aleksandra"
-        ]
+        let names = DataManager.names.shuffled()
+        let surnames = DataManager.surnames.shuffled()
+        let phoneNumbers = DataManager.phoneNumbers.shuffled()
+        let emails = DataManager.emails.shuffled()
 
-        let surnames: Set<String> = [
-            "Sokol",
-            "Oryol",
-            "Solovey",
-            "Sova",
-            "Zayac",
-            "Medved",
-            "Lisa",
-            "Volk"
-        ]
-
-        for (name, surname) in zip(names, surnames) {
-            let phoneNumber = Int.random(in: 100_000_000...999_999_999)
-            let email = "\(name.lowercased())_\(surname.lowercased())@mail.com"
-
+        for index in 0..<names.count {
             let person = Person(
-                name: name,
-                surname: surname,
-                phoneNumber: phoneNumber,
-                email: email
+                name: names[index],
+                surname: surnames[index],
+                phoneNumber: phoneNumbers[index],
+                email: emails[index]
             )
 
             persons.append(person)
